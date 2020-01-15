9aa20b3a3d5ea58dece28a10fddfd7854048c6d5
Narinrit <ifra@vista.aero>
2020-01-15 22:18:14 +0700

    Add vuetify and clean code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
+    extends: [
+        'plugin:vue/recommended',
+        '@vue/airbnb',
+        '@vue/typescript',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error', 4, {
+            'SwitchCase': 1,
+        }],
+        'vue/html-indent': ['error', 4],
+        'vue/script-indent': ['error', 4, {
+            'switchCase': 1,
+        }],
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
